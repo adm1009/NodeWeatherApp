412a70617c4d829d74ceae6280f48ddb58878c90
Abhijeet <abhijeetmulik319@gmail.com>
2022-06-16 14:28:47 +0530

    unnecessery console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log("client side js running!");
+// console.log("client side js running!");
 // fetch("http://localhost:8000/weather?address=boston").then((response)=>{
 //     response.json().then((data)=>{
 //         if(data.error){
